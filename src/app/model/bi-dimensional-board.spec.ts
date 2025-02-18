559022fa59a18f55b32c86a424795b91d2834471
geektortoise <geektortoise@users.noreply.github.com>
2025-02-18 17:01:25 +0000

    deploy: 17d994bc53db33c68738d04f18f0a58388b26eac

--- src/app/model/bi-dimensional-board.spec.ts
+++
@@ -1,7 +0,0 @@
-import { BiDimensionalBoard } from './bi-dimensional-board';
-
-describe('BiDimensionalBoard', () => {
-  it('should create an instance', () => {
-    expect(new BiDimensionalBoard(10, 10)).toBeTruthy();
-  });
-});
--- transloco.config.ts
+++
@@ -1,9 +0,0 @@
-import {TranslocoGlobalConfig} from '@jsverse/transloco-utils';
-
-const config: TranslocoGlobalConfig = {
-  rootTranslationsPath: 'src/assets/i18n/',
-  langs: [ 'en', 'fr' ],
-  keysManager: {}
-};
-
-export default config;
